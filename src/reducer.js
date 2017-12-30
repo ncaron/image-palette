4bfd8a29257f413aff64c9ccc33f69a4228f066e
Niko Caron <niko.caron90@gmail.com>
2017-12-30 05:26:04 -0400

    Basic Skeleton

---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+import * as types from './actionTypes';
+import initialState from './initialState';
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
